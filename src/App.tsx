2363969bebdcc8aa330cb714ddac92ac73eb7103
khil94 <khilkhil@naver.com>
2022-08-19 00:09:21 +0900

    feat : 기초 레이아웃 디자인

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainLayout from './components/MainLayout';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="register" element={<div>hwdawdawdi</div>} />
+          <Route path="hi" element={<div>hi</div>} />
+          <Route
+            path="*"
+            element={
+              <div>
+                <p>There is nothing here!</p>
+              </div>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,52 @@
+import { Outlet, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Box, Tab, Tabs } from '@mui/material';
+
+const MainLayout: React.FC = () => {
+  const [idx, setIdx] = useState<string>('/');
+  const navigation = useNavigate();
+
+  const handleIndexChange = (event: React.SyntheticEvent<Element, Event>, newIdx: string) => {
+    setIdx(newIdx);
+    navigation(newIdx);
+
+    console.log(newIdx);
+  };
+
+  return (
+    <Box>
+      <Box
+        sx={{
+          flexGrow: 1,
+          bgcolor: 'background.paper',
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        <Tabs
+          orientation="horizontal"
+          value={idx}
+          variant="scrollable"
+          onChange={handleIndexChange}
+          aria-label="Horizontal tabs"
+        >
+          <Tab label="Home" value={'/'} />
+          <Tab label="Register" value={'/register'} />
+          <Tab label="Item Three" value={'/dd'} />
+          <Tab label="Item Four" value={'/test'} />
+        </Tabs>
+      </Box>
+      <Box
+        sx={{
+          margin: '12px',
+          padding: '12px'
+        }}
+      >
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default MainLayout;
