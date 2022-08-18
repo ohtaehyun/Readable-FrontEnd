1936f93f93ca44b2bd0b52c4551e03ad1a473216
khil94 <khilkhil@naver.com>
2022-08-19 04:06:33 +0900

    feat : 회원가입 페이지 생성만

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route path="register" element={<div>hwdawdawdi</div>} />
+          <Route path="register" element={<RegisterPage />} />
           <Route path="hi" element={<div>hi</div>} />
           <Route
             path="*"
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import { Box, Button } from '@mui/material';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const RegisterPage: React.FC = () => {
+  const navigation = useNavigate();
+
+  return <Box>RegisterPage</Box>;
+};
+
+export default RegisterPage;
