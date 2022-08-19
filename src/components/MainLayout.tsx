a1ced86d35ea1770afe8418d2b54b81393176e3b
khil94 <khilkhil@naver.com>
2022-08-20 05:26:09 +0900

    feat : sideMenu 기본 골격 제작

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,7 +1,18 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
-import { Box, Button, Menu, Slide, Tab, Tabs, useMediaQuery } from '@mui/material';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  Menu,
+  Slide,
+  Switch,
+  Tab,
+  Tabs,
+  useMediaQuery
+} from '@mui/material';
 import styled from '@emotion/styled';
+import { MainMenuItemList } from '../constants/mainMenuItemList';
 
 const MainLayout: React.FC = () => {
   const [idx, setIdx] = useState<string>('/');
@@ -16,24 +27,24 @@ const MainLayout: React.FC = () => {
   };
 
   const handleShowSideMenu = () => {
-    setShowSideMenu(!showSideMenu);
+    setShowSideMenu(showSideMenu => !showSideMenu);
   };
 
   useEffect(() => {
+    console.log('hi');
     if (!matches) {
       setShowSideMenu(false);
     }
   }, [matches]);
 
-  const CommonLineBarNav = () => {
+  const NavBar = () => {
     return (
       <>
         <Box sx={{ paddingLeft: '24px' }}>
           <Tabs orientation="horizontal" value={idx} onChange={handleIndexChange}>
-            <Tab label="Home" value={'/'} />
-            <Tab label="Register" value={'/register'} />
-            <Tab label="Item Three" value={'/dd'} />
-            <Tab label="Item Four" value={'/test'} />
+            {MainMenuItemList.map(item => (
+              <Tab key={item.url} label={item.name} value={item.url} />
+            ))}
           </Tabs>
         </Box>
         <Box
@@ -52,25 +63,27 @@ const MainLayout: React.FC = () => {
 
   return (
     <Box>
-      <SideMenu direction="left" in={showSideMenu}>
-        <Box>hi</Box>
-      </SideMenu>
-      <Box
-        sx={{
-          flexGrow: 1,
-          bgcolor: 'background.paper',
-          width: '100vw',
-          height: '48px',
-          display: 'flex',
-          justifyContent: 'space-between'
-        }}
+      {/* 사이드메뉴, 따로 함수로 빼면 매번 재선언되어서 out animation이 정상 동작하지 않아 직접기입 */}
+      <SideMenuContainer
+        onExit={() => console.log('exit')}
+        onExiting={() => console.log('exit')}
+        mountOnEnter
+        appear
+        unmountOnExit
+        direction="left"
+        in={showSideMenu}
       >
-        {matches ? (
-          <Button onClick={() => handleShowSideMenu()}>showSideMenu</Button>
-        ) : (
-          <CommonLineBarNav />
-        )}
-      </Box>
+        <Box>
+          {MainMenuItemList.map(item => (
+            <li key={item.url}>
+              <Link to={item.url}>{item.name}</Link>
+            </li>
+          ))}
+        </Box>
+      </SideMenuContainer>
+      <NavBarContainer>
+        {matches ? <Button onClick={handleShowSideMenu}>ShowSideMenu</Button> : <NavBar />}
+      </NavBarContainer>
 
       <Box
         sx={{
@@ -86,10 +99,28 @@ const MainLayout: React.FC = () => {
 
 export default MainLayout;
 
-const SideMenu = styled(Slide)`
+const SideMenuContainer = styled(Slide)`
   position: fixed;
-  width: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 200px;
   height: 100%;
   right: 0px;
-  background-color: black;
+  top: 49px;
+  padding: 20px;
+  background-color: white;
+  border-left: 1px solid black;
+`;
+
+const NavBarContainer = styled(Box)`
+  flex-grow: 1;
+  background-color: white;
+  width: 100%;
+  height: 48px;
+  display: flex;
+  justify-content: space-between;
+  height: 48px;
+  border-bottom: 1px solid black;
 `;
